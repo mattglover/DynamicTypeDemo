412a40e3e6e10cf4e658171ae2d85b1797dd2d35
Matt Glover <matt@duchysoftware.com>
2015-09-24 18:07:06 +0100

    Initial Commit

---
+++ AutoLayoutDynamicTypeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutDynamicTypeDemo
+//
+//  Created by Matt Glover on 24/09/2015.
+//  Copyright © 2015 Duchy Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutDynamicTypeDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutoLayoutDynamicTypeDemo
+//
+//  Created by Matt Glover on 24/09/2015.
+//  Copyright © 2015 Duchy Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
